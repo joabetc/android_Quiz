f06f6f5e48bff53cb4521f3bc920fd77ea5e5c3c
retina <retina@Joabe.local>
2019-02-28 15:34:38 -0300

    Creating the activities

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPlay;
+    private Button btnRegister;
+    private Button btnListAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnListAll = findViewById(R.id.btnListAll);
+
     }
 }
